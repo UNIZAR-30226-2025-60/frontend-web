40802259c5532fb199ca205322462e86f17632d7
Javiersusin <872847@unizar.es>
2025-02-14 19:00:36 +0100

    pantalla base foro

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginComponent from "@/components/LoginComponent.vue";
 import InicioComponent from "@/components/InicioComponent.vue";
+import ForoComponent from "@/components/ForoComponent.vue";
 
 const routes = [
   { path: "/", name: "Login", component: LoginComponent },
   { path: "/inicio", name: "Inicio", component: InicioComponent },
+  { path: "foro", name: "Foro", component: ForoComponent },
 ];
 
 const router = createRouter({
