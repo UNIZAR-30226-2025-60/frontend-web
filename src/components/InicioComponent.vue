38ec576cf867265f086083dee90bc5155820a0ca
Lucía Vázquez Martín <871886@unizar.es>
2025-02-15 12:43:59 +0100

    Categorias

--- src/components/InicioComponent.vue
+++ src/components/InicioComponent.vue
@@ -32,6 +32,20 @@ export default {
 <template>
   <div v-if="user">
     <NavBar></NavBar>
+    <div>
+      <form class="align-items-center" role="search" style="margin: 0 10px;">
+        <div class="input-group">
+          <input class="form-control rounded-pill" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success rounded-pill" type="submit">Search</button>
+        </div>
+      </form>
+      <h4>Categorías:</h4>
+      <div class="row g-2">
+        <div v-for="tema in temas" :key="tema.tematica" class="col-1">
+          <button class="btn btn-outline-success rounded-pill btn-sm w-100">{{ tema.tematica }}</button>
+        </div>
+      </div>
+    </div>
     <div v-for="libro in libros" :key="libro.enlace" class="col-md-3 col-sm-4 col-6">
       <a href="#">
         <img class="img fluid" v-bind:src="libro.imagen_portada"
@@ -49,10 +63,37 @@ export default {
 
 <script>
 import axios from "axios";
+import NavBar from '@/components/NavBar.vue'
 export default {
+  components: {
+    NavBar
+  },
   data() {
     return {
       user: null, // Inicializamos con null
+      //Para probar botones de categorias
+      temas : [
+        {tematica: 'Amor'},
+        {tematica: 'Aventura'},
+        {tematica: 'Terror'},
+        {tematica: 'Comedia'},
+        {tematica: 'Fantasía'},
+        {tematica: 'Drama'},
+        {tematica: 'Acción'},
+        {tematica: 'Ciencia Ficción'},
+        {tematica: 'Romántico'},
+        {tematica: 'Misterio'},
+        {tematica: 'Thriller'},
+        {tematica: 'Historia'},
+        {tematica: 'Ficción'},
+        {tematica: 'Infantil'},
+        {tematica: 'Suspenso'},
+        {tematica: 'Psicológico'},
+        {tematica: 'Terror Psicológico'},
+        {tematica: 'Romántico'},
+        {tematica: 'Superhéroes'}
+      ],
+      libros: []
     };
   },
   async mounted() {
@@ -62,6 +103,7 @@ export default {
         withCredentials: true, // Permite enviar cookies al backend
       });
       this.user = response.data; // Guardamos los datos del usuario
+      this.cargarLibros();
     } catch (error) {
       console.error("Error al obtener los datos del usuario:", error);
       // Si hay error, redirigimos al login
@@ -80,3 +122,13 @@ export default {
     }
 };
 </script>
+<style scoped>
+  h4 {
+    margin-bottom: 10px;
+  }
+
+  button {
+    font-size: 0.8rem;  /* Ajustamos el tamaño del texto en los botones */
+    padding: 8px 16px;  /* Ajustamos el padding de los botones */
+  }
+</style>
