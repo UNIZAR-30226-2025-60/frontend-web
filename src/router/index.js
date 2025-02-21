5cd2d7ea54d1419c681cc0a97ef84cf0b52656d8
Javiersusin <872847@unizar.es>
2025-02-21 13:26:37 +0100

    ya6

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
-import { createRouter, createWebHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router"; // Cambio aquí
+
 import LoginComponent from "@/components/LoginComponent.vue";
 import InicioComponent from "@/components/InicioComponent.vue";
 import ForoComponent from "@/components/ForoComponent.vue";
@@ -7,19 +9,38 @@ import AvisoLegalComponent from "@/components/AvisoLegalComponent.vue";
 import PoliticaComponent from "@/components/PoliticaComponent.vue";
 import ContactoComponent from "@/components/ContactoComponent.vue";
 
-const routes = [
-  { path: "/", name: "Login", component: LoginComponent },
-  { path: "/inicio", name: "Inicio", component: InicioComponent },
-  { path: "/foro", name: "Foro", component: ForoComponent },
-  { path: "/detalles/:id", name: "Detalles", component: DetallesComponent },
-  { path: "/avisoLegal", name: "AvisoLegal", component: AvisoLegalComponent },
-  { path: "/politica", name: "Politica", component: PoliticaComponent },
-  { path: "/contacto", name: "Contacto", component: ContactoComponent },
-];
+// const routes = [
+//   { path: "/", name: "Login", component: LoginComponent },
+//   { path: "/inicio", name: "Inicio", component: InicioComponent },
+//   { path: "/foro", name: "Foro", component: ForoComponent },
+//   { path: "/detalles/:id", name: "Detalles", component: DetallesComponent },
+//   { path: "/avisoLegal", name: "AvisoLegal", component: AvisoLegalComponent },
+//   { path: "/politica", name: "Politica", component: PoliticaComponent },
+//   { path: "/contacto", name: "Contacto", component: ContactoComponent },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHashHistory(), // Cambio aquí
+  routes: [
+    { path: "/", name: "Login", component: LoginComponent },
+    { path: "/inicio", name: "Inicio", component: InicioComponent },
+    { path: "/foro", name: "Foro", component: ForoComponent },
+    { path: "/detalles/:id", name: "Detalles", component: DetallesComponent },
+    { path: "/avisoLegal", name: "AvisoLegal", component: AvisoLegalComponent },
+    { path: "/politica", name: "Politica", component: PoliticaComponent },
+    { path: "/contacto", name: "Contacto", component: ContactoComponent },
+  ],
 });
 
 export default router;
+
+
+
+
+
